68591f9ca9f54715fd4a3a596a066b23b80b05b5
svyrydenko <olexii.svyrydenko@amadeus.com>
2017-10-23 11:00:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Laravel auth helper
+
+![Version](https://poser.pugx.org/ollyxar/laravel-auth/v/stable.svg)
+![Downloads](https://poser.pugx.org/ollyxar/laravel-auth/d/total.svg)
+![License](https://poser.pugx.org/ollyxar/laravel-auth/license.svg)
+
+Get user's id from session identified by cookie
---
+++ src/Auth.php
@@ -0,0 +1,48 @@
+<?php namespace Ollyxar\LaravelAuth;
+
+use Illuminate\Encryption\Encrypter;
+use Illuminate\Auth\SessionGuard;
+
+/**
+ * Class FileAuth
+ * @package Ollyxar\LaravelAuth
+ */
+class FileAuth
+{
+    /**
+     * Get userId by request headers.
+     *
+     * @param array $headers
+     * @return bool|int
+     */
+    public static function getUserIdByHeaders(array $headers)
+    {
+        $cookieName = env(
+            'SESSION_COOKIE',
+            str_slug(env('APP_NAME', 'laravel'), '_') . '_session'
+        );
+
+        if (!isset($headers['Cookie'])) {
+            return false;
+        }
+
+        preg_match("/(^|;)\s*$cookieName\s*=\s*([^;|^\n]+)/", $headers['Cookie'], $match);
+
+        if (!isset($match[2]) || empty($match[2])) {
+            return false;
+        }
+
+        $cookie = urldecode($match[2]);
+        $key = base64_decode(explode(':', env('APP_KEY'))[1]);
+
+        $sessionName = (new Encrypter($key, config('app.cipher')))->decrypt($cookie);
+
+        if (!$sessionFile = @file_get_contents(config('session.files') . '/' . $sessionName)) {
+            return false;
+        } else {
+            $object = @unserialize($sessionFile);
+            $userId = @$object['login_web_' . sha1(SessionGuard::class)];
+            return $userId;
+        }
+    }
+}
